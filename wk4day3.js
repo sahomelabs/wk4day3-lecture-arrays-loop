66e1bb668bfe5259f7639b5fe364359a2f1ff291
sahomelabs <sahomelabs@gmail.com>
2023-10-27 11:45:42 -0400

    Yell at the NinjaTurtles answered

--- wk4day3.js
+++ wk4day3.js
@@ -56,27 +56,33 @@
 // Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
 // Give D'Art a second hometown by adding "Hawkins"
 // Porgee decides that Wolfy can't be named "Wolfy" anymore. Remove "Wolfy" from the wolfyarray and replace it with "Gameboy".
-    const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
-    const sharky = ["Sharky", "shark", 20, "Left Coast"]
-    const plantee = ["Plantee", "plant",  5000 , "Mordor"]
-    const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
-    const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+//     const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+//     const sharky = ["Sharky", "shark", 20, "Left Coast"]
+//     const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+//     const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+//     const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
-// Plantee just had her birthday; change Plantee's array to reflect her being a year older.
-        plantee[2]++;
+// // Plantee just had her birthday; change Plantee's array to reflect her being a year older.
+//         plantee[2]++;
 
-// Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
-        wolfy[3] = "Gotham City";
-
-
-// Give D'Art a second hometown by adding "Hawkins"
-        dart[3] += ", Hawkins";
-
-// Remove "Wolfy" from the wolfyarray and replace it with "Gameboy"        
-wolfy[0] = "Gameboy"
-
-console.log(wolfy)
+// // Change Wolfy's hometown from "Yukon Territory" to "Gotham City".
+//         wolfy[3] = "Gotham City";
 
 
+// // Give D'Art a second hometown by adding "Hawkins"
+//         dart[3] += ", Hawkins";
+
+// // Remove "Wolfy" from the wolfyarray and replace it with "Gameboy"        
+// wolfy[0] = "Gameboy"
+
+// console.log(wolfy)
+
+// Yell at the Ninja Turtles
+// Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+const ninjaTurtles = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+// Use a for of loop(not a typo - try it out! Try a for ofloop) to call toUpperCase()on each of them and print out the result.
+for (const turles of ninjaTurtles) {
+    console.log(turles.toUpperCase());
+}
 
 
