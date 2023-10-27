cad91fa28d479089067fd3d3182b67c5b2e94f62
sahomelabs <sahomelabs@gmail.com>
2023-10-27 10:55:40 -0400

    Fizz Buzz answered

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
     Practice with Arrays
     Practice with Loops
     Practice with Conditionals
+
+// Fizz Buzz
+    This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
+    Write a javascript application that logs all numbers from 1 - 100.
+    If a number is divisible by 3 log "Fizz" instead of the number.
+    If a number is divisible by 5 log "Buzz" instead of the number.
+    If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
--- wk4day3.js
+++ wk4day3.js
@@ -4,7 +4,42 @@
 // }
 
 //Write a for loop that will log only the even numbers in 0 through 200.Write a for loop that will log only the even numbers in 0 through 200.
-for (let i = 0; i <= 200; i += 2){
-    console.log(i);
+// for (let i = 0; i <= 200; i += 2){
+//     console.log(i);
+// }
+
+// 1. Write a javascript application that logs all numbers from 1 - 100.
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// 2. If a number is divisible by 3 log "Fizz" instead of the number.
+// for (let i =1; i <=100; i++) {
+//     if (i % 3 === 0) {
+//         console.log("Fizz");
+//         } else {
+//             console.log(i); 
+//         }
+// }
+// 3. If a number is divisible by 5 log "Buzz" instead of the number
+// for (let i=1; i<=100; i++) {
+//     if (i % 5 === 0) {
+//         console.log("Buzz");
+//         } else {
+//             console.log(i);
+//     }
+// }    
+// 4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+            } else if (i % 5 === 0) {
+                console.log("Buzz");
+                } else {
+                    console.log(i);
+                    }
 }
+
 
